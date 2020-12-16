541d1f5656e74ae246b7c656811f60d58f14ec7e
arnold-graf <arnold.graf@gmail.com>
2020-12-16 17:40:48 +0100

    add parent ids

--- src/dao/tag.queries.ts
+++ src/dao/tag.queries.ts
@@ -16,6 +16,7 @@ export interface ISelectTagsResult {
   tagEbeneName: string;
   tagEbeneId: number;
   childIds: numberArray | null;
+  parentIds: numberArray | null;
 }
 
 /** 'SelectTags' query type */
