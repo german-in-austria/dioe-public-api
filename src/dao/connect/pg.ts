bf2fcc8ded39281e45433d8db3a8d724df1533f0
arnold-graf <arnold.graf@gmail.com>
2020-12-17 14:02:16 +0100

    use pg conf for connection

--- src/dao/connect/pg.ts
+++ src/dao/connect/pg.ts
@@ -1,5 +1,6 @@
 import { TaggedQuery } from '@pgtyped/query'
-import { Pool, types, Client } from 'pg'
+import { Pool, types } from 'pg'
+import { db } from '../../../pgconfig.json'
 
 // set type parser for numeric types since pg connect would parse numeric (float) as string
 types.setTypeParser(1700, 'text', parseFloat)
@@ -8,11 +9,11 @@ const slowDuration = process.env.PG_SLOW_DURATION || 250 // milliseconds
 console.log(`slow query duration: ${slowDuration}ms`)
 
 const pool = new Pool({
-  database: process.env.PG_DATABASE,
-  host:     process.env.PG_HOST,
-  port:     Number(process.env.PG_PORT),
-  user:     process.env.PG_USER,
-  password: process.env.PG_PASSWORD
+  database: db.dbName,
+  host:     db.host,
+  port:     db.port,
+  user:     db.user,
+  password: db.password
 })
 
 export default async function<T extends { params: any, result: any }>(q: TaggedQuery<T>, params?: T['params']) {
