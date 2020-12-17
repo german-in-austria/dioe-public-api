ed46f3011ec26bb26900effb57f58e5364f68abc
arnold-graf <arnold.graf@gmail.com>
2020-12-17 14:02:10 +0100

    logger

---
+++ src/lib/log.ts
@@ -0,0 +1,24 @@
+import util from 'util'
+import colors from 'colors/safe'
+
+if (process.env.NODE_ENV === 'development') {
+  require('source-map-support').install()
+}
+const log = console.log
+
+const clog = function(...args: any[]){
+  if (process.env.NODE_ENV === 'development') {
+    let obj: any = {}
+    // show line numbers during development
+    Error.captureStackTrace(obj, clog)
+    const lineNumber = obj.stack.split('\n')[1] // .match(/\(\/.{1,}\)/)
+    log.apply(console, args.map( a => util.inspect(a, { depth: 4 })).concat('\n' + colors.dim(lineNumber)))
+  } else {
+    log.apply(console, args)
+  }
+}
+
+console.log = clog
+console.error = clog
+console.info = clog
+console.warn = clog
