94684eea75ac10def1c7939b0850940ca1a0eb68
Jakob Bal <baljakob9@gmail.com>
2022-04-13 12:32:19 +0200

    Extended validate

--- src/service/validate.ts
+++ src/service/validate.ts
@@ -1,10 +1,22 @@
 import _, { String } from "lodash";
+import { antwortenDto } from "src/controller/antwortenController";
 import { tagDto } from "src/controller/tagController";
 import { ausbildungGrad } from "./social";
 
 export interface tag {
   ids: number[];
   erhArt: number[];
+  ausbildung: string;
+  beruf_id: number;
+  weiblich: string;
+}
+
+export interface ageBound {
+  ageLower: number;
+  ageUpper: number;
+}
+
+export interface filters extends ageBound {
   ausbildung: string;
   beruf_id: number;
   weiblich: string;
@@ -25,6 +37,20 @@ export default {
     } as tag;
     return res;
   },
+  validateAntwortenDto(ant: antwortenDto): filters {
+    const ageBound = this.validateAgeBound(ant.ageLower, ant.ageUpper);
+    const aus = this.validateAusbildung(ant.ausbildung ? ant.ausbildung : "");
+    const beruf = this.validateBeruf(ant.beruf_id ? ant.beruf_id : -1);
+    const gender = String(ant.weiblich);
+
+    return {
+      ausbildung: aus,
+      beruf_id: beruf,
+      weiblich: gender,
+      ageLower: ageBound.ageLower,
+      ageUpper: ageBound.ageUpper,
+    } as filters;
+  },
   validateAusbildung(str: string): string {
     let aus = ausbildungGrad.find((el: string) => el === str);
     if (aus == undefined) {
@@ -34,5 +60,16 @@ export default {
   },
   validateBeruf(nr: number) {
     return nr < 0 ? -1 : nr;
+  },
+  validateAgeBound(
+    lower: number | undefined,
+    upper: number | undefined
+  ): ageBound {
+    const low = lower ? lower : -1;
+    const up = upper ? upper : -1;
+    return {
+      ageLower: low,
+      ageUpper: up,
+    } as ageBound;
   },
 };
