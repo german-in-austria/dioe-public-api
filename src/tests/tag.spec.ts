78fd101fe4897b18c9e759f2a092610a5f634ce5
Jakob Bal <baljakob9@gmail.com>
2022-04-26 14:05:27 +0200

    Wrote further tests for tagController

--- src/tests/tag.spec.ts
+++ src/tests/tag.spec.ts
@@ -4,6 +4,15 @@ import server from "../server";
 import { Server } from "http";
 import { after, endsWith } from "lodash";
 import { tagDto } from "src/controller/tagController";
+// import { Ausbildungsgrad } from "src/enums/enums";
+
+enum Ausbildungsgrad {
+  pflicht = "pflichtschule",
+  reife = "hochschulreife",
+  abschluss = "hochschulabschluss",
+  beruf = "berufsausbildung",
+  empty = "",
+}
 
 let app: Server;
 
@@ -37,7 +46,26 @@ describe("GET /api/tags", () => {
   });
 });
 
-describe("GET /api/tags/ort", () => {
+describe("GET /api/tags/ort/{tagId}", () => {
+  it("Fetch Tag using single endpoint, Get same results as multiple endpoint", (done) => {
+    const id = 3;
+    request(app)
+      .get(`/api/tags/ort/${id}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.headers["content-type"]).toEqual(
+          expect.stringContaining("json")
+        );
+        const first = res.body[0];
+        expect(res.body.length).toBeGreaterThan(1);
+        expect(first.numTag).toBe("419");
+        done();
+      });
+  });
+});
+
+describe("POST /api/tags/ort", () => {
   it("Fetch Single Tag using multiple endpoint, Get results", (done) => {
     const tagBody: tagDto = {
       ids: [3],
@@ -61,8 +89,11 @@ describe("GET /api/tags/ort", () => {
 
   it("Fetch Single Tag using multiple endpoint with ErhebungsArt, Get results", (done) => {
     const tagBody: tagDto = {
-      ids: [3],
+      ids: [4],
       erhArt: [1],
+      ausbildung: Ausbildungsgrad.pflicht,
+      beruf_id: 16,
+      weiblich: true,
     } as tagDto;
     request(app)
       .post("/api/tags/ort")
@@ -75,8 +106,30 @@ describe("GET /api/tags/ort", () => {
         );
         const first = res.body[0];
         expect(res.body.length).toBeGreaterThanOrEqual(1);
-        console.log(first);
-        expect(first.numTag).toBe("30");
+        expect(+first.numTag).toBeGreaterThanOrEqual(122);
+        expect(first.tagName).toBe("SDent");
+        expect(first.tagId).toBe(4);
+        done();
+      });
+  });
+
+  it("Fetch Single Tag using multiple endpoint with non existent erhArt, Should be treated as -1", (done) => {
+    const tagBody: tagDto = {
+      ids: [3],
+      erhArt: [35],
+    } as tagDto;
+    request(app)
+      .post("/api/tags/ort")
+      .send(tagBody)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.headers["content-type"]).toEqual(
+          expect.stringContaining("json")
+        );
+        const first = res.body[0];
+        expect(res.body.length).toBeGreaterThan(1);
+        expect(first.numTag).toBe("419");
         done();
       });
   });
